34c6b6149fcdcdf7e7b01b5c73dab73be365d853
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-09-18 12:49:32 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
 
-    let response = await fetch('https://script.google.com/macros/s/AKfycbwqFaANnu-rUMnkuIADebfV8E1FuD-akKccVO875efeJcmN9xwXdzyjwFQkSgKjSEh2/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbxLOea0QX5Y6u0rA7WWJGyUFU_r-9kIAuQ7Ty2YVvhpXIkABg1CD8DZUhC2qZhUtTxQsA/exec', {
       method: 'POST',
       body: formData
     })
